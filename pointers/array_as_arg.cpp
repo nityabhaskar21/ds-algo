d6378306e64c20ddd3cb4d15ff846a88949c944c
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-06-25 13:09:39 +0530

    Added pass array as reference.

--- pointers/array_as_arg.cpp
+++ pointers/array_as_arg.cpp
@@ -9,6 +9,7 @@ int sumUsingArrayArg(int A[]) {  //Here we pass the array as reference only. We 
 
     int size = sizeof(A)/sizeof(A[0]);
     // Here 'sizeof' on array function parameter 'A' will return size of 'int*' and not of array.
+    // So its size will be 4 bytes or 8 bytes depending on machine architecture.
 
     cout<<"Size of array when passed as fun arg in sumUsingArrayArgWithoutSize: "<<size<<endl;
     for (i=0; i<size; i++) {
@@ -28,6 +29,19 @@ int sumUsingArrayArgWithSize(int A[], int size) {
     return sum;
 }
 
+int sumUsingArrayArgWithReference(int (&A)[8]) {  
+    // Here f(int & A[100]);  is an error as it means 
+    // This is parsed as an "array of references" - which isn't legal.
+
+    int i, sum = 0;
+    int size = sizeof(A)/sizeof(A[0]);
+    cout<<"Size of array when passed as fun arg in sumUsingArrayArgWithSize: "<<size<<endl;
+    for (i=0; i<size; i++) {
+        sum += A[i];
+    }
+    return sum;
+}
+
 int main() {
     int A[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int totalByArrayArgWithoutSize = sumUsingArrayArg(A);
@@ -35,4 +49,7 @@ int main() {
 
     int totalByArrayArgWithSize = sumUsingArrayArgWithSize(A, sizeof(A)/ sizeof(A[0]));
     cout<<"Sum of array when passed as fun arg: "<<totalByArrayArgWithSize<<endl;
+
+    int totalByArrayArgWithReference = sumUsingArrayArgWithReference(A);
+    cout<<"Sum of array when passed as fun reference: "<<totalByArrayArgWithReference<<endl;
 }
