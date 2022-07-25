21641c5f5ec5d4128018c6939663284b2d984b6f
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-07-25 18:09:29 +0530

    Added maximun integer

--- binary_numbers/maximun_integer_value.cpp
+++ binary_numbers/maximun_integer_value.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main() {
 
     // 1<<n is equivalent to 2^n
-    
+
     cout<<INT_MAX<<endl;
 
     int a = (1LL<<31)-1;
     cout<<a<<endl;
 
-    unsigned int b = (1LL<<32)-1;
+    int b = (1LL<<32)-1;
     cout<<b<<endl;
+
+    unsigned int c = (1LL<<32)-1;
+    cout<<c<<endl;
 
 
     return 0;
