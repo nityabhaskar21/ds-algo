faca8bbb47bad6c72da2444e7777f919bd7a9753
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-07-25 18:08:27 +0530

    Added max integer value

---
+++ binary_numbers/maximun_integer_value.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>  
+
+using namespace std;
+
+int main() {
+
+    // 1<<n is equivalent to 2^n
+    
+    cout<<INT_MAX<<endl;
+
+    int a = (1LL<<31)-1;
+    cout<<a<<endl;
+
+    unsigned int b = (1LL<<32)-1;
+    cout<<b<<endl;
+
+
+    return 0;
+}
