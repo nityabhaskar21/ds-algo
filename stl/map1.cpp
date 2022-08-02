2e3c5cfa712386464108b856f63f572ff25b9a52
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-08-02 15:08:14 +0530

    Added map example using iterators

--- stl/map1.cpp
+++ stl/map1.cpp
@@ -69,6 +69,13 @@ int main()
         cout<<e.first<<" "<<e.second<<endl;
     }
 
+    unordered_map<int, string>::iterator it = m1.begin();
+
+    for (it = m1.begin(); it!=m1.end(); it++) {
+        cout<<(*it).first <<" "<< it->second<<endl;
+        // Here (*it).first === it->first
+    }
+
 
     return 0;
 }
