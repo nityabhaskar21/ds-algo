2232a661d403b1585bccbb6adbcaf3dd48e25175
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-08-07 02:44:02 +0530

    Added max and min element in array and vector

---
+++ stl/max_andmin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a[5] = { 0, 7, 1, 2, 3};
+    int min_ele1 = *min_element(a, a+5);    //min_element return a pointer
+    cout<<min_ele1<<endl;
+
+    vector<int> v{7, 1, 2, 3};
+    int max_ele2 = *max_element(v.begin(), v.end()); 
+    cout<<max_ele2<<endl;
+
+    return 0;
+}
