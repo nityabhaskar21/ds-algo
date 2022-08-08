f2fe75464feaa1eb8af76f15bb8f4c274cfbd113
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-08-08 10:00:28 +0530

    Added upper bound algorithm.

--- stl/set_upper_and_lower_bound.cpp
+++ stl/set_upper_and_lower_bound.cpp
@@ -1,6 +1,7 @@
 // set::lower_bound/upper_bound
 #include <iostream>
 #include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -11,8 +12,13 @@ int main ()
 
   for (int i=1; i<10; i++) myset.insert(i*10); // 10 20 30 40 50 60 70 80 90
 
+  //Also works for map as well in O(logn) complexity.
   itlow=myset.lower_bound (30);                //       ^
   itup=myset.upper_bound (60);                 //                   ^
+
+
+
+  cout<<"Algotithm: "<<*upper_bound(myset.begin(),myset.end(), 60)<<endl; //This also works but time complexity is O(n)
 
   cout<<"lower_bound: "<<" "<<*itlow<<" "<<endl;
   cout<<"upper_bound: "<<" "<<*itup<<" "<<endl;
