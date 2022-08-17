c9e4839082e51be3ec4b338ad9f13bc179190444
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-08-17 12:28:14 +0530

    Added bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,92 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+int MOD_INT = 1e9+7;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+// Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements and 
+// then swapping their positions if they exist in the wrong order.
+
+void bubble_sort(int A[ ], int n ) {
+    int temp;
+    for(int k = 0; k< n-1; k++) {
+        // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
+
+        for(int i = 0; i < n-k-1; i++) {
+            if(A[ i ] > A[ i+1] ) {
+                // here swapping of positions is being done.
+                temp = A[ i ];
+                A[ i ] = A[ i+1 ];
+                A[ i + 1] = temp;
+            }
+        }
+    }
+}
+ 
+
+int main()
+{
+    fast_cin();
+
+    // #ifndef ONLINE_JUDGE
+    //    freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+    // #endif
+
+    int A[] = { 0, 1, 9, 6, 4, 5, 6,};
+    bubble_sort(A, sizeof(A)/sizeof(A[0]));
+
+    for (int i = 0; i < sizeof(A)/sizeof(A[0]); i++) {
+        cout << A[ i ] <<" ";
+    }
+
+    return 0;
+}
