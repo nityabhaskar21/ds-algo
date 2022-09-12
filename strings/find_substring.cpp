6805174448fb5fad785338133e4c35d0f3c7b2c8
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-09-12 12:43:27 +0530

    Added find substring

--- strings/find_substring.cpp
+++ strings/find_substring.cpp
@@ -13,7 +13,12 @@ int main()
     string str1= "fruit Mango is my favorite fruit";  
     cout <<  str1<<'\n';  
     cout<< "position of fruit is from position 12 :";  
-    cout<< str1.find("fruit",12);  
+    cout<< str1.find("fruit",12)<<endl;  
+
+    //If no matches were found, the function returns string::npos.
+    //npos is a static member constant value with the greatest possible value for an element of type size_t.
+    cout<< "position of vegatable is  :";  
+    cout<< str1.find("vegatable");  
 
     return 0;   
 }   
