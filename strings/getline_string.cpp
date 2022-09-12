37c2ed2bf4a6be0656c72c67f487f404dd9f22dc
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-09-12 12:17:52 +0530

    Added string getline

---
+++ strings/getline_string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char profession[12];
+  
+  cout << "Enter your profession: ";
+
+  //The getline() function is used to read a string entered by the user. 
+  // The getline() function extracts characters from the input stream. 
+  // It adds it to the string object until it reaches the delimiting character. 
+  // The default delimiter is \n. 
+  // The delimiter could either be a character or the number of characters a user can input.
+
+  // We will take maximum 12 characters as input
+  cin.getline(profession, 12);
+  
+  cout << "Your profession is: " << profession;
+  
+  return 0;
+}
