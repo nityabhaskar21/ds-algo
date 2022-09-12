d2827072fddeec031b7c8607412b3234efe94c03
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-09-12 15:18:08 +0530

    Added rounding numbers

---
+++ mathematics/rounding_numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+// Driver function
+int main()
+{
+    // using floor function which return
+    // floor of input value
+    cout << "Floor is : " << floor(2.3) << endl;
+    cout << "Floor is : " << floor(-2.3) << endl;
+
+
+    // using ceil function which return
+    // floor of input value
+    cout << "Ceil is : " << ceil(2.3) << endl;
+    cout << "Ceil is : " << ceil(-2.3) << endl;
+
+
+    // using round function which return
+    // round of input value
+    cout << "Round is : " << round(2.3) << endl;
+    cout << "Round is : " << round(-2.3) << endl;
+ 
+    return 0;
+}
