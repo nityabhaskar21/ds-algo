dbbba027a4721d013ddc9d46df63e98dfb74fafd
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-09-15 14:33:37 +0530

    Added n^3 sol for longest palindrome

---
+++ strings/manachers_algorithm/find_palindrome_using_substring.cpp
@@ -0,0 +1,103 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+int MOD_INT = 1e9+7;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+string longest_palindrome(string str) {
+    int l = str.length();
+
+    int startPalindromeIndex = 0;
+    int endPalindromeIndex = l-1;
+    int lengthPalindrome = 0;
+
+    for (int i = 0; i < l; i++) {
+        for (int j = i+1; j < l; j++) {
+            int s = i;
+            int e = j;
+            bool flag = true;
+
+            while (s < e) {
+                if (str[s] != str[e]) {
+                    flag = false;
+                }
+                s++;
+                e--;
+            }
+            if (flag) {
+                if (j-i+1 > lengthPalindrome) {
+                    lengthPalindrome = j-i+1;
+                    startPalindromeIndex = i;
+                    endPalindromeIndex = j;
+                }
+            }
+        }
+    }
+
+    return str.substr(startPalindromeIndex, lengthPalindrome);
+}
+
+int main()
+{
+    fast_cin();
+
+    #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+    #endif
+
+    string str;
+    cin>>str;
+    cout<<longest_palindrome(str);
+
+
+    return 0;
+}
