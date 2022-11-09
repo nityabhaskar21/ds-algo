a96da3cbe0355bb0ddcc3207054a0212b9fee63b
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-11-09 12:46:09 +0530

    Added string int conversion

---
+++ mathematics/int_to_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+#include<string>  
+using namespace std; 
+
+
+int main()  {  
+
+    int i=11;  
+    float f=12.3;  
+    string str= to_string(i);  
+    string str1= to_string(f);  
+    cout<<"string value of integer i is :"<<str<<"\n";  
+    cout<<"string value of f is : "<< str1;  
+
+    return 0;
+}  
