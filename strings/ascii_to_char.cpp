e981280bf2366474efe32811722558d41be7f762
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2023-07-16 11:22:51 +0530

    Added ascii to char

---
+++ strings/ascii_to_char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+using namespace std; 
+
+int main() {
+    
+
+    int i1=65;
+    cout<<i1<<", char:"<<char(i1)<<endl;
+
+
+    int i2=102;
+    char c2 = char(i2);
+    cout<<i2<<", char:"<<c2<<endl;
+
+
+    return 0;
+}
